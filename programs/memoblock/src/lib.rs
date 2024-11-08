5dce28a6630dd06d4f570e529611ef4f5a1257d0
Amit Kumar <amitkcodes@gmail.com>
2024-11-09 01:04:44 +0530

    created the 'memory account' and 'create memory' context struct

--- programs/memoblock/src/lib.rs
+++ programs/memoblock/src/lib.rs
@@ -6,11 +6,45 @@ declare_id!("62yyqxq4mfFatbWwMjXh7hn4149RFCZm7RxMNcWChZjN");
 pub mod memoblock {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create_memory(
+        ctx: Context<CreateMemory>,
+        title: String,
+        description: String,
+    ) -> Result<()> {
+        let memory_account = &mut ctx.accounts.memory_account;
+        memory_account.owner = ctx.accounts.payer.key();
+        memory_account.title = title;
+        memory_account.description = description;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(title: String)]
+pub struct CreateMemory<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = payer,
+        space = Memory::INIT_SPACE,
+        seeds = [title.as_bytes(), payer.key().as_ref()],
+        bump,
+    )]
+    pub memory_account: Account<'info, Memory>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Memory {
+    pub owner: Pubkey,
+
+    #[max_len(100)]
+    pub title: String,
+
+    #[max_len(1000)]
+    pub description: String,
+}
